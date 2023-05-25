7616f65cd74b360400cdb9dee55c2952aef02cba
capivaracritica <133689058+capivaracritica@users.noreply.github.com>
2023-05-25 16:59:04 -0300

    atividade dia 25

---
+++ ex4/scripts/meuprimeiroscript.js
@@ -0,0 +1,12 @@
+/* window.alert('seja bem vindo')
+window.confirm('vamos confirmar seus dados?')
+window.prompt('qual é o seu nome?')
+console.log('ola!seja bem vindo a nossa aula de javascript')
+ */ /* vocês podem usar aspas simples, aspas ou aspas duplas ou acento grave */
+
+ var nome =('Qual é seu nome ?')
+ /* recebe */
+ /* esse sinal= não esiguinifica igualno javascript.aquip sinal de iguadade se escreve assim,== */
+
+ window.alert(`seja bem vindo! ${nome}`)
+ console.log(`seja bem vindo! ${nome}`)
---
+++ ex4/scripts/operador.js
@@ -0,0 +1,21 @@
+var valor1 = 2
+var valor2 = 5
+var valor3 = 10
+var valor4 = Number.parseInt('1')
+
+/* numeros não precisa de aspas */
+
+console.log(typeof valor1)
+console.log(typeof valor4)
+
+ var resultado = (valor4) + valor1 + valor2 
+
+ console.log(typeof valor4)
+
+
+ /* quando tentamos realizar uma soma entre um number e uma string o javascrpt faz uma concatenação */
+
+ console.log(`O resultado da operação é : ${resultado}`)
+
+ 
+ 
